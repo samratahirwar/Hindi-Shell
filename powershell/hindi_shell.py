89ccbe55662a2c121a57c9f42de0a497e3b73b90
Samrat Ahirwar <93487468+samratahirwar@users.noreply.github.com>
2022-05-16 23:30:40 +0530

    os sep

---
+++ powershell/hindi_shell.py
@@ -0,0 +1,38 @@
+import sys
+import subprocess
+from prog_dict import prog_dict
+import os
+from englisttohindi.englisttohindi import EngtoHindi
+
+def translate(sent):
+    trans = EngtoHindi(message=sent)
+    return trans.convert
+
+def convert(command):
+    new_command=None
+    try:
+        command=command.split(' ')
+        new_command=prog_dict[command[0].strip()]+' '+' '.join(command[1:])
+    except KeyError:
+            print(command)
+            prog_dict[command[0].strip()]=''
+            return None
+    return new_command
+
+command=None
+while True:
+    command=str(input('॥ '),)
+    if command=='अन्त':
+        break
+    if command.strip()==None:
+        continue
+    command=convert(command)
+    if command==None:
+        continue
+    try:
+        output=subprocess.check_output(command, shell=True, text=True)
+    except Exception as e:
+        output=translate(str(e))
+    print(translate(output))
+
+print('॥ समाप्त ॥')
